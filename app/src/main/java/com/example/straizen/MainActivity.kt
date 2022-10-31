9058f78939555d7733694fb5a6569914d4c44d16
Fareed Matovu <fareedmkim@gmail.com>
2022-10-31 08:30:18 +0300

    refinement on homepage

--- app/src/main/java/com/example/straizen/MainActivity.kt
+++ app/src/main/java/com/example/straizen/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.straizen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val loginbutton: Button = findViewById(R.id.login_button)
+
+        loginbutton.setOnClickListener {
+            val login = Intent(this, Dashboard::class.java )
+            startActivity(login)
+        }
     }
 }

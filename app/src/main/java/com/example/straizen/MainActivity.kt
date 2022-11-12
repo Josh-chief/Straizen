c0af4ce4381222b8e5280de30dbf32bb7856f619
Fareed Matovu <fareedmkim@gmail.com>
2022-11-12 12:52:18 +0300

    App finally working again

--- app/src/main/java/com/example/straizen/MainActivity.kt
+++ app/src/main/java/com/example/straizen/MainActivity.kt
@@ -4,17 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.example.stratizen.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val loginbutton: Button = findViewById(R.id.login_button)
+        val loginbutton: Button = findViewById(R.id.submitBtn)
 
         loginbutton.setOnClickListener {
-            val login = Intent(this, Dashboard::class.java )
-            startActivity(login)
+
+
         }
     }
 }

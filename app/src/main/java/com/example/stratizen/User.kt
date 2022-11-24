e584ce4f568eccdd1f5379f68eaac2813ea413d4
Fareed Matovu <fareedmkim@gmail.com>
2022-11-24 15:07:26 +0300

    SignUp page working

---
+++ app/src/main/java/com/example/stratizen/User.kt
@@ -0,0 +1,11 @@
+package com.example.stratizen
+
+
+data class User (
+
+    val fullname: String,
+    val studentnumber:String,
+    val email:String
+
+
+)
--- app/src/main/java/com/example/stratizen/signup_page.kt
+++ app/src/main/java/com/example/stratizen/signup_page.kt
@@ -1,8 +1,10 @@
 package com.example.stratizen
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -11,6 +13,7 @@ import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class signup_page : AppCompatActivity() {
@@ -24,6 +27,8 @@ class signup_page : AppCompatActivity() {
         setContentView(R.layout.activity_signup_page)
          var createAccont: Button = findViewById(R.id.btnCreate)
          var alreadyUser: TextView = findViewById(R.id.userAlready)
+
+
 
 
 
@@ -58,8 +63,33 @@ class signup_page : AppCompatActivity() {
         if(inputUsername.isEmpty() || inputStudentNumber.isEmpty()||inputEmail.isEmpty() || inputPassword.isEmpty()){
             Toast.makeText(this, "Fill all fields", Toast.LENGTH_LONG).show()
         }else{
-            var login = Intent(this, MainActivity::class.java)
-            startActivity(login)
+
+            auth.createUserWithEmailAndPassword(inputEmail, inputPassword)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+
+                       var user = User(inputUsername,inputStudentNumber,inputEmail)
+                        val database = Firebase.database
+                        val myRef = database.getReference("User")
+
+                        myRef.setValue(user)
+
+                        username.editText?.text?.clear()
+                        studentNo.editText?.text?.clear()
+                        email.editText?.text?.clear()
+                        password.editText?.text?.clear()
+
+                        Toast.makeText(baseContext, "Registration Successful",
+                            Toast.LENGTH_SHORT).show()
+
+                        var intent = Intent(this,MainActivity::class.java)
+                        startActivity(intent)
+
+                        } else {
+                        Toast.makeText(baseContext, "Registration failed",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }
         }
     }
 
